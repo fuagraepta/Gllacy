e63dd714ab8659774c3ba7f42ec6e7713026a773
Vladimir <simpleHelpD@yandex.ru>
2020-10-29 16:19:01 +1000

    Минификация стилей и скриптов

--- js/feedback.js
+++ js/feedback.js
@@ -1,22 +1,37 @@
 'use strict';
 
 (function () {
-
+  const ESC_CODE = 27;
   const buttonMapContacts = document.querySelector(`.button--map-contacts`);
   const feedback = document.querySelector(`.modal-feedback`);
   const feedbackForm = feedback.querySelector(`.modal-feedback__form`);
   const feedbackClose = feedback.querySelector(`.modal-feedback__close`);
   const feedbackName = feedback.querySelector(`.modal-feedback__name`);
   const feedbackEmail = feedback.querySelector(`.modal-feedback__email`);
+  const feedbackDiscription = feedback.querySelector(`.modal-feedback__discription`);
 
   const openPopup = (popup) => {
     popup.classList.add(`modal__show`);
     feedbackName.focus();
+    document.addEventListener(`keydown`, onEscClosePopup);
   };
 
   const closePopup = (popup) => {
     popup.classList.remove(`modal__show`);
+    document.removeEventListener(`keydown`, onEscClosePopup);
   };
+
+  const onEscClosePopup = (escEvt) => {
+    if (escEvt.keyCode === ESC_CODE) {
+      closePopup(feedback);
+    }
+  }
+
+  const elementStopPropagation = (evt) => {
+    if (evt.keyCode === ESC_CODE) {
+      evt.stopPropagation();
+    }
+  }
 
   buttonMapContacts.addEventListener(`click`, (evt) => {
     evt.preventDefault();
@@ -28,6 +43,11 @@
     closePopup(feedback);
     feedback.classList.remove(`modal__error`);
   });
+
+  // Don't close the popup on esc press if one of the following fields is focuse
+  feedbackName.addEventListener(`keydown`, elementStopPropagation);
+  feedbackEmail.addEventListener(`keydown`, elementStopPropagation);
+  feedbackDiscription.addEventListener(`keydown`, elementStopPropagation);
 
   // Restart the animation trick
   let restartAnimation = () => feedback.offsetWidth;
